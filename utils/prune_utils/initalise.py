1bf949858ad8afd7d9d5bd3d8ae89f3fa37d30cf
Elliott Piercy <elliottpiercy@hotmail.co.uk>
2020-05-20 00:12:49 +0100

    magnitude pruning and initialisation

--- utils/prune_utils/initalise.py
+++ utils/prune_utils/initalise.py
@@ -1,9 +1,9 @@
 import utils.prune_utils.magnitude_pruning
 
 
-def scheduler(schedule_type, schedule_config):
+def scheduler(pruning_config):
     
-    if schedule_type == 'magnitude':
-        schedule = utils.prune_utils.magnitude_pruning.schedule(schedule_config)
+    if pruning_config['schedule_type'] == 'magnitude':
+        schedule = utils.prune_utils.magnitude_pruning.schedule(pruning_config)
         return schedule
     
--- utils/prune_utils/magnitude_pruning.py
+++ utils/prune_utils/magnitude_pruning.py
@@ -1,11 +1,19 @@
 import numpy as np
 
-# Magnitude weight pruning schedule
+
+"""
+Magnitude weight pruning schedule - Mask all weights that are within the threshold.
+
+EG: threshold = 0.1. ALl weights between -0.1 and 0.1 are masked with 0s
+
+"""
+
+
 class schedule():
     
     
-    def __init__(self, threshold):
-        self.threshold = threshold
+    def __init__(self, pruning_config):
+        self.threshold = pruning_config['threshold']
     
     
     # Update mask for weight vector based on new weights
@@ -26,6 +34,24 @@ class schedule():
             masks[ID] = mask
             
         return masks
+    
+    
+    # Apply magnitude pruning schedule
+    def apply(self, model, sess):
+        
+        # If the threshold is 0 -- ignore and dont prune
+        if self.threshold != 0:
+            masks = self.update_mask(model.weights, lbound=-self.threshold, ubound=self.threshold)
+
+            for ID in masks.keys():
+                new_weights = model.masks[ID] * model.weights[ID]
+                sess.run(model.weights[ID].assign(new_weights))
+                
+            return masks
+        
+        else:
+            return model.masks
+
     
     
         
